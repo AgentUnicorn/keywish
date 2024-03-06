d4186b874dffb2df208b954390b103bbfb3e082e
Binh (Dev) <superslap1999@gmail.com>
2024-03-06 23:09:37 +0700

    export image

--- controllers/WishlistController.js
+++ controllers/WishlistController.js
@@ -44,6 +44,25 @@ wishlist.controller("WishlistController", function($scope, $uibModal, WishlistSe
         });
     }
 
+    $scope.openExportModal = function()
+    {
+        const modalInstance = $uibModal.open({
+            templateUrl: 'partials/ExportModal.html', // Path to your modal content template
+            controller: 'ExportModalController', // Controller for the modal instance
+            size: 'lg', // Optional size (e.g., 'sm', 'lg')
+            windowClass: 'show',
+            resolve: {
+                wishlist: () => $scope.sections
+            }
+        });
+
+        modalInstance.result.then(function(result) {
+            // Handle modal close (optional)
+        }, function() {
+            // Handle modal dismiss (optional)
+        });
+    }
+
     $scope.editTitle = function(section) {
         section.editedTitle = angular.copy(section.title);
         $scope.editing = true;
@@ -78,51 +97,5 @@ wishlist.controller("WishlistController", function($scope, $uibModal, WishlistSe
             $scope.sections[2] = section
             localStorage.setItem('sections', JSON.stringify($scope.sections));
         }, 500); // Adjust the delay (in milliseconds) as needed
-    };
-
-    $scope.preview = function (param) 
-    {  
-
-    }
-
-    $scope.exportToPNG = function() {
-        let template = document.getElementById('export-template').innerHTML;
-        // console.log(template)
-        let canvas = document.createElement('canvas');
-        let ctx = canvas.getContext('2d');
-
-        // Render HTML content to canvas
-        html2canvas(document.getElementById('export-container'), {
-            onrendered: function(canvas) {
-                var imgData = canvas.toDataURL('image/png');
-                var link = document.createElement('a');
-                link.download = 'exported-data.png';
-                link.href = imgData;
-                link.click();
-            }
-        });
-    };
-
-    $scope.element = angular.element(document.getElementById('export-template'));
-    $scope.getCanvas = null;
-
-    $scope.previewImage = function() {
-        console.log(document.getElementById('export-template'))
-        html2canvas($scope.element, {
-            onrendered: function(canvas) {
-                angular.element(document.getElementById('previewImage')).append(canvas);
-                $scope.getCanvas = canvas;
-            }
-        });
-    };
-
-    $scope.convertHtmlToImage = function() {
-        if ($scope.getCanvas) {
-            var imgageData = $scope.getCanvas.toDataURL("image/png");
-            var newData = imgageData.replace(/^data:image\/png/, "data:application/octet-stream");
-
-            var downloadLink = angular.element(document.getElementById('btn-Convert-Html2Image'));
-            downloadLink.attr("download", "GeeksForGeeks.png").attr("href", newData);
-        }
     };
 });
